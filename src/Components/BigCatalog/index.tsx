1467a00d600f7d14b6254e582e2854ed4544f96b
Gogisung <gogosing@inetcop.org>
2020-02-21 16:32:48 +0900

    BigCatalog 수정 함

--- src/Components/BigCatalog/index.tsx
+++ src/Components/BigCatalog/index.tsx
@@ -1,2 +1,50 @@
 import React, {useState,useEffect} from 'react';
 import {FlatList} from 'react-native';
+import Styled from 'styled-components/native';
+import BigCatalog from '~/Components/BigCatalog';
+
+const Container = Styled.View`
+  height: 300px;
+  margin-bottom: 8px;
+`;
+
+interface Props {
+  url: string;
+  onPress: (id: number) => void;
+}
+
+const BigCatalogList = ({url, onPress}: Props) => {
+  const [data, setData] = useState<Array<IMovie>>([]);
+  useEffect(() => {
+    fetch(url)
+      .then(response => response.json())
+      .then(json => {
+        console.log(json);
+        setData(json.data.movies);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }, []);
+
+  return (
+    <Container>
+      <FlatList
+        horizontal={true}
+        pagingEnabled={true}
+        data={data}
+        keyExtractor={(item, index) => {
+          return `bigScreen-${index}`;
+        }}
+        renderItem={({item, index}) =>(
+          <BigCatalog
+            id={(item as IMovie).id}
+            
+          />
+        )}
+      />
+    </Container>
+  )
+};
+
+export default BigCatalogList;
