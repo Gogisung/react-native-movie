f28af62bb1beee6a591807e34756ce9c05ebe299
Gogisung <ggs0707@naver.com>
2020-02-22 15:08:03 +0900

    BigCatalog 만들기

---
+++ src/Screens/MovieHome/BigCatalogList/index.tsx
@@ -0,0 +1,16 @@
+import React, {useState, useEffect} from 'react';
+import {FlatList} from 'react-native';
+import Styled from 'styled-components/native';
+
+import BigCatalog from '~/Components/BigCatalog';
+
+interface Props {
+  url: string;
+  onPress: (id: number) => void;
+}
+
+const BigCatalogList = ({url, onPress} :Props) => {
+  return (
+
+  );
+};
