33cbaabc0d094aab76c58ead004ba2f29cd888ca
Gogisung <gogosing@inetcop.org>
2020-02-24 09:49:45 +0900

    무비디테일 만들기

--- src/Screens/MovieDetail/index.tsx
+++ src/Screens/MovieDetail/index.tsx
@@ -1,7 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import {ActivityIndicator} from 'react-native';
 import Styled from 'styled-components/native';
-
 import {NavigationScreenProp, NavigationState} from 'react-navigation';
 
 import BigCatalog from '~/Components/BigCatalog';
@@ -45,7 +44,23 @@ interface Props {
 
 const MovieDetail = ({navigation}: Props) => {
   const [data, setData] = useState<IMovieDetail>();
-  return (
+
+  useEffect(() => {
+    const id = navigation.getParam('id');
+    fetch(
+      `https://yts.lt/api/v2/movie_details.json?movie_id=${id}&with_images=true&with_cast=true`
+    )
+      .then(response => response.json())
+      .then(json => {
+        console.log(json);
+        setData(json.data.movie);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }, []);
+
+  return data ? (
     <Container>
       <BigCatalog
         id={data.id}
@@ -54,8 +69,44 @@ const MovieDetail = ({navigation}: Props) => {
         title={data.title}
         genres={data.genres}
       />
+      <SubInfoContainer>
+        <ContainerTitle>영화 정보</ContainerTitle>
+        <InfoContainer>
+          <LabelInfo>{data.runtime}분</LabelInfo>
+          <LabelInfo>평점: {data.rating}</LabelInfo>
+          <LabelInfo>좋아요: {data.like_count}</LabelInfo>
+        </InfoContainer>
+      </SubInfoContainer>
+      <DescriptionContainer>
+        <ContainerTitle>줄거리</ContainerTitle>
+        <Description>{data.description_full}</Description>
+      </DescriptionContainer>
+      {data.cast && <CastList cast={data.cast} />}
+      <ScreenShotList
+        images={[
+          data.large_screenshot_image1,
+          data.large_screenshot_image2,
+          data.large_screenshot_image3,
+        ]}
+      />
     </Container>
+  ) : (
+    <LoadingContainer>
+      <ActivityIndicator size="large" color="#E70915" />
+    </LoadingContainer>
   );
+};
+
+MovieDetail.navigationOptions = {
+  title: 'MOVIEAPP',
+  headerTintColor: '#E70915',
+  headerStyle: {
+    backgroundColor: '#141414',
+    borderBottomWidth: 0,
+  },
+  headerTitleStyle: {
+    fontWeight: 'bold',
+  },
 };
 
 export default MovieDetail;
